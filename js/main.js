093ff0d975a99c24681cbadcedd261f36835ceb9
andriiwp3 <andriiwp3@gmail.com>
2020-08-03 23:21:25 +0300

    Made a hero section

--- js/main.js
+++ js/main.js
@@ -188,4 +188,59 @@ if (iconMenu) {
 		menuBody.classList.toggle("active");
 	});
 }
+
+
+
+/**
+ * 
+ * [description]
+ * 
+ */
+$('.categories__title').on('click', () => {
+	if ($('.categories__title').hasClass('active')) {
+		$('.categories__title').next().slideDown(300);
+	} else {
+		$('.categories__title').next().slideUp(300);
+	}
+
+	$('.categories__title').toggleClass('active');
+	$('.categories__icon').toggleClass('active');
+})
+
+
+
+/**
+ * 
+ * Логика для спойлеров
+ *
+ */
+$.each($('.spoller.active'), function (index, val) {
+	$(this).next().show();
+});
+$('body').on('click', '.spoller', function (event) {
+
+	$(this).parent().toggleClass('active');
+
+	if (this.querySelector('.icon-menu')) {
+		this.querySelector('.icon-menu').classList.toggle('active');
+	}
+
+	if ($(this).parents('.one').length > 0) {
+		$(this).parents('.one').find('.spoller').not($(this)).removeClass('active').next().slideUp(300);
+		$(this).parents('.one').find('.spoller').not($(this)).parent().removeClass('active');
+	}
+
+	if ($(this).hasClass('closeall') && !$(this).hasClass('active')) {
+		$.each($(this).closest('.spollers').find('.spoller'), function (index, val) {
+			$(this).removeClass('active');
+			$(this).next().slideUp(300);
+		});
+	}
+	$(this).toggleClass('active').next().slideToggle(300, function (index, val) {
+		if ($(this).parent().find('.slick-slider').length > 0) {
+			$(this).parent().find('.slick-slider').slick('setPosition');
+		}
+	});
+	return false;
+});
 });
